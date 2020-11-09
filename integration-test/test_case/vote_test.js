e0fe8715d099a67e1a316fc6614063cddc66a2af
SantiagoMerlo <santiago.agustin.merlo@gmail.com>
2020-11-09 17:52:14 -0300

    update CI

--- integration-test/test_case/vote_test.js
+++ integration-test/test_case/vote_test.js
@@ -6,4 +6,7 @@ Scenario('Testing Vote View', ({ I }) => {
     I.amOnPage('https://is3-vote.herokuapp.com/')
     I.see('CATS')
     I.see('DOGS')
+    I.seeElement('//*[@id="content-container-center"]/h3')
+    I.seeElement('//*[@id="a"]')
+    I.seeElement('//*[@id="b"]')
 });
