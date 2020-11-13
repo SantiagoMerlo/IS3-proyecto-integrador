908b76c9e9c61e628e0fc9bfde806f7460899383
SantiagoMerlo <santiago.agustin.merlo@gmail.com>
2020-11-13 16:44:51 -0300

    worker is working :) :)

--- worker/src/main/java/worker/Worker.java
+++ worker/src/main/java/worker/Worker.java
@@ -8,25 +8,22 @@ import org.json.JSONObject;
 
 class Worker {
 
-  private static String DB_URL = "postgres://uzmqbqluddcolu:2a2fc77c8108ef5b4fce2e77d8742c577641a3c9952dc99b6485ccb7e6d343b4@ec2-18-233-32-61.compute-1.amazonaws.com:5432/d57p3q8jvbo598"; // System.getenv("DATABASE_URL")
+  private static String POST_HOST = System.getenv("REDIS_HOST");
 
-  private static String POST_HOST = "ec2-18-233-32-61.compute-1.amazonaws.com"; // System.getenv("REDIS_HOST")
+  private static String POST_DB = System.getenv("POST_DB");
 
-  private static String POST_DB = "d57p3q8jvbo598"; // System.getenv("POST_DB")
+  private static String POST_PASS = System.getenv("POST_PASS");
 
-  private static String POST_PASS = "2a2fc77c8108ef5b4fce2e77d8742c577641a3c9952dc99b6485ccb7e6d343b4";
+  private static String POST_PORT = System.getenv("POST_PORT");
 
-  private static String POST_PORT = "5432"; // System.getenv("POST_PORT")
+  private static String POST_USER = System.getenv("POST_USER");
 
-  private static String POST_URI = "postgres://uzmqbqluddcolu:2a2fc77c8108ef5b4fce2e77d8742c577641a3c9952dc99b6485ccb7e6d343b4@ec2-18-233-32-61.compute-1.amazonaws.com:5432/d57p3q8jvbo598"; // System.getenv("POST_URI")
+  private static String REDIS_HOST = System.getenv("REDIS_HOST");
 
-  private static String POST_USER = "uzmqbqluddcolu"; // System.getenv("POST_USER")
+  private static String REDIS_PASS = System.getenv("REDIS_PASS");
 
-  private static String REDIS_HOST = "redis-17663.c16.us-east-1-3.ec2.cloud.redislabs.com"; //System.getenv("REDIS_HOST")
+  private static String REDIS_PORT = System.getenv("REDIS_PORT");
 
-  private static String REDIS_PASS = "0g4WbPSX6ilxJRBkMBmYIWNS2D8I7ShZ";// System.getenv("REDIS_PASS")
-  
-  private static String REDIS_PORT = "17663"; // System.getenv("REDIS_PORT")
   public static void main(String[] args) {
     try {
 
@@ -58,27 +55,25 @@ class Worker {
   }
 
   static void updateVote(Connection dbConn, String voterID, String vote) throws SQLException {
-    PreparedStatement insert = dbConn.prepareStatement(
-      "INSERT INTO votes (id, vote) VALUES (?, ?)");
+    PreparedStatement insert = dbConn.prepareStatement("INSERT INTO votes (id, vote) VALUES (?, ?)");
     insert.setString(1, voterID);
     insert.setString(2, vote);
 
     try {
       insert.executeUpdate();
     } catch (SQLException e) {
-      PreparedStatement update = dbConn.prepareStatement(
-        "UPDATE votes SET vote = ? WHERE id = ?");
+      PreparedStatement update = dbConn.prepareStatement("UPDATE votes SET vote = ? WHERE id = ?");
       update.setString(1, vote);
       update.setString(2, voterID);
       update.executeUpdate();
     }
   }
 
-  static String redisPing( Jedis conn) {
+  static String redisPing(Jedis conn) {
     return "Server is runing:" + conn.ping();
   }
 
-  public static String strConnectionRedis(){
+  public static String strConnectionRedis() {
     return "redis://default:" + REDIS_PASS + "@" + REDIS_HOST + ":" + REDIS_PORT;
   }
 
@@ -110,7 +105,7 @@ class Worker {
       conn = DriverManager.getConnection(url);
 
       PreparedStatement st = conn.prepareStatement(
-        "CREATE TABLE IF NOT EXISTS votes (id VARCHAR(255) NOT NULL UNIQUE, vote VARCHAR(255) NOT NULL)");
+          "CREATE TABLE IF NOT EXISTS votes (id VARCHAR(255) NOT NULL UNIQUE, vote VARCHAR(255) NOT NULL)");
       st.executeUpdate();
 
     } catch (ClassNotFoundException e) {
@@ -123,10 +118,9 @@ class Worker {
   }
 
   public static String strConnectionPostgres() {
-    return "jdbc:postgresql://" + POST_HOST + ":" + POST_PORT + "/" + POST_DB + "?user=" + POST_USER + "&password=" + POST_PASS;
+    return "jdbc:postgresql://" + POST_HOST + ":" + POST_PORT + "/" + POST_DB + "?user=" + POST_USER + "&password="
+        + POST_PASS + "&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
   }
-
-
 
   static void sleep(long duration) {
     try {
